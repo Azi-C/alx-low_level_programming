edb429aa3b3b63fe0133e08ad4bacb92eedd8391
Azi-C <aaziza.chbihi@gmail.com>
2023-09-13 07:55:21 -0700

    function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,17 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
-		_putchar("+");
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
 		 return (0);
-		 _putchar("0");
+		 _putchar('0');
 	}
 	else
 	{
 		 return (-1);
-		 _putchar("-");
+		 _putchar('-');
 	}
 }
 
