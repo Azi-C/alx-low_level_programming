0e655dae7021675478d8e621c38cc681b0fece4d
Azi-C <aaziza.chbihi@gmail.com>
2023-09-13 10:41:46 -0700

    function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,38 @@
 #include "main.h"
 
 /**
- * times_table- function that prints the 9 times table, starting with 0
+ * times_table - prints the 9 times table, starting with 0
  */
 
 void times_table(void)
 {
-	int i = 0;
-	int j = 0;
-	
-	while (i < 11)
+	int b;
+	int m;
+	int p;
+
+	for (b = 0; b <= 9; b++)
 	{
-		while (j < 11)
+		for (m = 0; m <= 9; m++)
 		{
-			_putchar(i*j+','+'\t');
-			j++;
+			p = (b * m);
+			if (m == 0)
+				_putchar('0');
+			else if (p <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + p);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (p / 10));
+				_putchar('0' + (p % 10));
+			}
 		}
-		i++;
 		_putchar('\n');
 	}
+}
 
