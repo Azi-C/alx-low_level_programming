b9761fa26f7f14dfc41c7fc9187e5f2de22dc0e4
Azi-C <aaziza.chbihi@gmail.com>
2023-09-14 15:28:37 -0700

    . Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,25 +12,19 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100 ;i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
 		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
+			printf("Buzz");
 		else
-		{
-			printf("%i ", i);
-		}
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
-	retuen (0);
+	return (0);
 }
