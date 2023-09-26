8d0b4f7d54c3c499eae8bdfc49fd1193947aeb5b
Azi-C <aaziza.chbihi@gmail.com>
2023-09-26 16:41:46 -0700

    function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: array with the size 8
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%d", a[i][j]);
+		}
+
+		printf("\n");
+	}
+}
