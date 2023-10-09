d62a8231dfb60c20d1716c78eb243bd2cb14842c
Azi-C <aaziza.chbihi@gmail.com>
2023-10-09 05:02:47 -0700

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -20,7 +20,7 @@ int *array_range(int min, int max)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= min - max; i++)
+	for (i = 0; i <= (min - max); i++)
 		*(ptr + i) = min + i;
 
 	return (ptr);
