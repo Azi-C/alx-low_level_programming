874b078b46ece4ccbf6753ab5f740ecb974429e0
Azi-C <aaziza.chbihi@gmail.com>
2023-10-11 12:22:36 -0700

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer of type dog
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+
+	exit (EXIT_SUCCESS);
+}
