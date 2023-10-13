baf76c112de12cc3293206fa674d6ece95a43b38
Azi-C <aaziza.chbihi@gmail.com>
2023-10-13 15:15:08 -0700

    contain the 5 functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+
+/**
+ * op_add - sum of a and b
+ * @a: first integer input
+ * @b: second integer input
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculates the difference of a and b
+ * @a: first integer input
+ * @b: second integer input
+ * Return: difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - calculates the product of a and b
+ * @a: first integer input
+ * @b: second integer input
+ * Return: product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculates the division of a by b
+ * @a: first integer input
+ * @b: second integer input
+ * Return: division of a by b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates the remainder of the division of a by b
+ * @a: first integer input
+ * @b: second integer input
+ * Return:  remainder of the division of a by b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
