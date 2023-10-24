0a7a45659abfb8f4f81558797f19562664ebe7f3
azich <aaziza.chbihi@gmail.com>
2023-10-24 14:55:57 -0700

    add a node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,13 +30,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	while (i < idx && temp)
+	while (i < idx - 1 && temp)
 	{
 		temp = temp->next;
 		i++;
 	}
 
-	if (i == idx)
+	if (i == idx - 1)
 	{
 		new->next = temp->next;
 		temp->next = new;
