290651e3c2f9e2ea78d49c580772be565dc69684
Azi-C <aaziza.chbihi@gmail.com>
2023-12-07 15:16:38 -0800

    modifications

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!head)
 		return (NULL);
 
-	new = malloc(sierof(dlistint_t));
+	new = malloc(sizeof(dlistint_t));
 
 	if (!new)
 		return (NULL);
