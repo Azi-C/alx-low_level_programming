a653b8a5fe96bd8bd23a250cfac94bfa29996716
Azi-C <aaziza.chbihi@gmail.com>
2023-12-07 16:51:28 -0800

    betty modifications

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_new(dlistint_t *tmp, int n)
  * @h: the pointer to the first node
  * @idx: the index xhere the node should be added
  * @n: the data
- * Retuen:  address of the new node, or NULL if it failed
+ * Return:  address of the new node, or NULL if it failed
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
