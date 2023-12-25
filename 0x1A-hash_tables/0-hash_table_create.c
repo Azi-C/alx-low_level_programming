de05eccb0c68affed58ded8b253bbdbbbb903ecf
Azi-C <aaziza.chbihi@gmail.com>
2023-12-24 16:32:14 -0800

    modifications

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create -  function that creates a hash table
+ * hash_table_create - function that creates a hash table
  * @size: array's size
  * Return: a pointer to the newly created hash table
  */
